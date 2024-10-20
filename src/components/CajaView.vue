4302bf360e5c99f03483d940c7f05a65d845f217
Alan <alan.gpagliardini@gmail.com>
2024-10-20 19:02:23 -0300

    creadas vistas y rutas

---
+++ src/components/CajaView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Caja</h1>
+    <!-- You can add other components here later -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CajaView',
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import ProductosView from "@/components/ProductosView.vue";
+import VentasView from "@/components/VentasView.vue";
+import ComprasView from "@/components/ComprasView.vue";
+import CajaView  from "@/components/CajaView.vue";
 
 const routes = [
-    {
-    },
+    {path: '/productos', component: ProductosView},
+    {path: '/ventas', component: VentasView},
+    {path: '/compras', component: ComprasView},
+    {path: '/caja', component: CajaView},
     // Otras rutas...
 ];
 
